08a08fed3f7be491169db07e7ca690f2a7253142
Alexander Shpilkin <ashpilkin@gmail.com>
2019-04-25 11:18:28 +0300

    Rename password hash according to documentation

--- dvrip.py
+++ dvrip.py
@@ -1,8 +1,8 @@
 from hashlib import md5
 from string  import ascii_lowercase, ascii_uppercase, digits
 
-HASHMAGIC = (digits + ascii_uppercase + ascii_lowercase).encode('ascii')
-def hash(password):
+MD5MAGIC = (digits + ascii_uppercase + ascii_lowercase).encode('ascii')
+def md5crypt(password):
 	mdfive = md5(bytes(password)).digest()
-	return bytes(HASHMAGIC[(a+b) % len(HASHMAGIC)]
+	return bytes(MD5MAGIC[(a+b) % len(MD5MAGIC)]
 	             for a, b in zip(mdfive[0::2], mdfive[1::2]))[:8]
