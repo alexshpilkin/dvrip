02853ea70e9adf1440da55c4003159065d587305
Alexander Shpilkin <ashpilkin@gmail.com>
2019-04-25 13:27:08 +0300

    Implement encoding for control packets

--- dvrip.py
+++ dvrip.py
@@ -1,8 +1,103 @@
 from hashlib import md5
 from string  import ascii_lowercase, ascii_uppercase, digits
+from struct  import Struct
+from sys     import intern
+
 
 MD5MAGIC = (digits + ascii_uppercase + ascii_lowercase).encode('ascii')
 def md5crypt(password):
 	mdfive = md5(bytes(password)).digest()
 	return bytes(MD5MAGIC[(a+b) % len(MD5MAGIC)]
 	             for a, b in zip(mdfive[0::2], mdfive[1::2]))[:8]
+
+
+class AbstractPacket(object):
+	MAGIC    = 0xFF
+	VERSION  = 0x01
+	__STRUCT = Struct('<BBxxII')
+
+	__slots__ = ('session', 'number')
+	def __init__(self, number=None, session=None, *, other=None):
+		super().__init__()
+
+		if isinstance(other, AbstractPacket):
+			assert number is None and session is None
+			number  = other.number
+			session = other.session
+		self.session = session
+		self.number  = number
+
+	@property
+	def size(self):
+		return self.__STRUCT.size
+
+	def encodeto(self, buffer, offset=0):
+		struct = self.__STRUCT
+		struct.pack_into(buffer, offset, self.MAGIC, self.VERSION,
+		                 self.session, self.number)
+		return memoryview(buffer)[offset+struct.size:]
+
+	def encode(self):
+		buf = bytearray(self.size)
+		self.encodeto(buf)
+		return buf
+
+	def chunks(self):
+		return (self.encode(),)
+
+	def dump(self, fp):
+		for chunk in self.chunks():
+			chunk = memoryview(chunk)
+			while chunk:
+				chunk = chunk[fp.write(chunk):]
+
+
+class AbstractControlPacket(AbstractPacket):
+	__STRUCT = Struct('<BBHI')
+
+	__slots__ = ('fragments', 'fragment', 'type', 'length')
+	def __init__(self, type=None, fragment=None, fragments=None,
+	             *args, other=None, **named):
+		super().__init__(*args, other=other, **named)
+
+		if isinstance(other, AbstractControlPacket):
+			assert (type is None and fragment is None and
+			        fragments is None)
+			type      = other.type
+			fragment  = other.fragment
+			fragments = other.fragments
+		self.fragments = fragments
+		self.fragment  = fragment
+		self.type      = type
+
+	@property
+	def size(self):
+		return super().size + self.__STRUCT.size + self.length
+
+	def encodeto(self, buffer, offset=0):
+		buffer = super().encodeto(buffer, offset)
+		struct = self.__STRUCT
+		struct.pack_into(buffer, 0, self.fragments, self.fragment,
+		                 self.type, self.length)
+		return buffer[struct.size:]
+
+
+class UnknownControlPacket(AbstractControlPacket):
+	__slots__ = ('data',)
+	def __init__(self, data=None, *args, other=None, **named):
+		super().__init__(*args, other=other, **named)
+
+		if isinstance(other, UnknownControlPacket):
+			assert data is None
+			data = other.data
+		self.data = data
+
+	@property
+	def length(self):
+		return len(self.data)
+
+	def encodeto(self, buffer, offset=0):
+		buffer = super().encodeto(buffer, offset)
+		data   = self.data
+		buffer[:len(data)] = data
+		return buffer[len(data):]
--- test_dvrip.py
+++ test_dvrip.py
@@ -1,7 +1,31 @@
 from dvrip import *
+from io    import BytesIO
 
 def test_md5crypt_empty():
 	assert md5crypt(b'') == b'tlJwpbo6'
 
 def test_md5crypt_tluafed():
 	assert md5crypt(b'tluafed') == b'OxhlwSG8'
+
+def test_AbstractPacket_encode():
+	p = AbstractPacket(0xdefa, 0xabcd)
+	assert p.encode().hex() == 'ff010000cdab0000fade0000'
+	assert p.size == len(p.encode())
+
+def test_AbstractPacket_dump():
+	b = BytesIO()
+	p = AbstractPacket(0xdefa, 0xabcd)
+	p.dump(b)
+	assert p.encode() == b.getvalue()
+
+def test_UnknownControlPacket_encode():
+	p = UnknownControlPacket(b'hello', 0x7856, 0x34, 0x12, 0xdefa, 0xabcd)
+	assert p.encode().hex() == ('ff010000cdab0000fade0000'
+	                            '123456780500000068656c6c6f')
+	assert p.size == len(p.encode())
+
+def test_UnknownControlPacket_dump():
+	b = BytesIO()
+	p = UnknownControlPacket(b'hello', 0x7856, 0x34, 0x12, 0xdefa, 0xabcd)
+	p.dump(b)
+	assert p.encode() == b.getvalue()
